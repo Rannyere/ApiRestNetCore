63fee85fba495b31c0053c006e8034dc6c5f38f8
Rannyere Almeida <rannyalmeida27@hotmail.com>
2020-12-09 12:37:23 +0100

    building ApiRest

---
+++ src/IO.Business/Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using IO.Business.Interfaces;
+using IO.Business.Models;
+using IO.Business.Validations;
+
+namespace IO.Business.Services
+{
+    public class ProductService : BaseService, IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository,
+                              INotifier notifier) : base(notifier)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task Add(Product product)
+        {
+            if (!ExecuteValidation(new ProductValidation(), product)) return;
+
+            await _productRepository.Add(product);
+        }
+
+        public async Task Update(Product product)
+        {
+            if (!ExecuteValidation(new ProductValidation(), product)) return;
+
+            await _productRepository.Update(product);
+        }
+
+        public async Task Remove(Guid productId)
+        {
+            await _productRepository.Remove(productId);
+        }
+
+        public void Dispose()
+        {
+            _productRepository?.Dispose();
+        }
+    }
+}
